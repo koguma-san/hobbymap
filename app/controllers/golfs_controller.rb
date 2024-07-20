7f83b115f22a6f3ab0db1ee96f4ae8a1728732e0
koguma-san <swymmrn.o.28@gmail.com>
2024-07-20 09:36:09 +0900

    ラウンド登録機能

--- app/controllers/golfs_controller.rb
+++ app/controllers/golfs_controller.rb
@@ -4,6 +4,7 @@ class GolfsController < ApplicationController
   def index
     @target = current_user.targets.last
     @records = current_user.records
+    @best_round = current_user.rounds.joins(score: :score_details).group('rounds.id').order('SUM(score_details.strokes) ASC').first
   end
 
   private
